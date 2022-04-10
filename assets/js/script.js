5e582d79f3b1153f4ea152e5ecb5a1382c659292
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-04-10 15:11:54 -0400

    progress on issue 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 // Display current date
-var today = document.querySelector('#currentDay');
+var today = document.getElementById('currentDay');
 currentDay = moment().format('dddd, MMMM Do');
 today.textContent = currentDay;
 
+// color-coding
+var row = document.querySelector('.row');
+var time = moment().format('h A');
+
+var hourDisplayed = document.getElementsByClassName('hour');
+for (i=0; i < (hourDisplayed.length)-1; i++){
+    if (hourDisplayed[i].innerHTML < time) {
+        row.setAttribute("class", "row col-9 past");
+        console.log(hourDisplayed[i].innerHTML)
+    }
+}
