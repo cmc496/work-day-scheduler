8c3dfa7b278ac88b0d01f54a99e1365479c5a163
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-04-18 17:36:53 -0400

    debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 // Display current date
 var today = document.getElementById('currentDay');
-currentDay = moment().format('dddd, MMMM Do');
-today.textContent = currentDay;
 
-// color-coding
-var row = document.querySelector('.row');
-var time = moment().format('h A');
-
-// var hourDisplayed = document.getElementById('hourZero');
-// for (i=0; i < (hourDisplayed.length); i++){
-//     if (hourDisplayed[i].innerHTML < time) {
-//         row.setAttribute("class", "row col-9 past");
-//         console.log(hourDisplayed[i].innerHTML)
-//     }
-// }
-
-var hourDisplayed = document.getElementById('hourZero').textContent;
-console.log(hourDisplayed);
-if (hourDisplayed < time) {
-    row.setAttribute("class", "row col-9 past");
-}
+$(document).ready(function() {
+    var currentDay = moment().format('dddd MMMM Do');
+    $("#currentDay").text(currentDay);
+function timeNow() {
+    var now = moment().hours()
+    var block = $(".time-block");
+    block.each(function() {
+        var hour = parseInt($(this).attr("id"))
+        if (hour === now) {
+            $(this).children(".description").attr("class", "present col-sm-10 description");
+        } else if (now > hour) {
+            $(this).children(".description").attr("class", "past col-sm-10 description");
+        } else {
+            $(this).children(".description").attr("class", "future col-sm-10 description");
+        }
+    })
+};
+timeNow();
+});
